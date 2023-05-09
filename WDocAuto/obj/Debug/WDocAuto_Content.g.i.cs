1e7549a0d8684dea989a52e9aeabd56f094c6135
Cynastic <pauldeppel@gmail.com>
2023-05-09 18:22:40 +0200

    0.2
    Icon and Style Changes
    More Functionality Added
    Bug Fixes

--- WDocAuto/obj/Debug/WDocAuto_Content.g.i.cs
+++ WDocAuto/obj/Debug/WDocAuto_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("savepath.txt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon.ico")]
 
 
