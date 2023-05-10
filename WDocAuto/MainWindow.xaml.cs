af36948c3606fe5e61a2a8ec1456157566cdf460
Cynastic <pauldeppel@gmail.com>
2023-05-10 19:55:39 +0200

    0.3
    Added Options Menu with:
      - Option to Toggle the Folder name in the File name
      - Option to Toggle Date and Folder Name Showing in Document
      - Option to change Size of the Title Font
    
    Fixed Bugs:
      - Files not saved as .docx
      - Cancellation of Filedialog breaks Path

--- WDocAuto/MainWindow.xaml.cs
+++ WDocAuto/MainWindow.xaml.cs
@@ -25,14 +25,21 @@ namespace WDocAuto
     /// </summary>
     public partial class MainWindow : Window
     {
-        string currentPath = string.Empty;
+        string currentPath = "C:\\";
         string SelectedPath = string.Empty;
         List<string> SubDirectories = new List<string>();
         List<string> SubDirectoryNames = new List<string>();
         string DateDay = DateTime.Now.Day > 9 ? DateTime.Now.Day.ToString() : "0" + DateTime.Now.Day.ToString();
         string DateMonth = DateTime.Now.Month > 9 ? DateTime.Now.Month.ToString() : "0" + DateTime.Now.Month.ToString();
+        string DateYear = DateTime.Now.Year.ToString().Substring(2);
+        
+        public string SavedPathFile = AppDomain.CurrentDomain.BaseDirectory + "SavePath.txt";
 
-        string SavedPathFile = AppDomain.CurrentDomain.BaseDirectory + "SavePath.txt";
+        //Options
+        bool FolderInTitle = false;
+        int TitleSize = 11;
+        bool IncludeFDate = true;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -46,34 +53,37 @@ namespace WDocAuto
             {
                 currentPath = ChangePathDialog();
             }
+            SavePathToFile();
             GetSubDirectories();
         }
 
         public string ChangePathDialog()
         {
             WinForms.FolderBrowserDialog folderBrowser = new WinForms.FolderBrowserDialog();
-            folderBrowser.ShowDialog();
-            
-            if(Directory.Exists(folderBrowser.SelectedPath))
+            if(folderBrowser.ShowDialog() == WinForms.DialogResult.OK)
             {
-                //Console.WriteLine("Set Path to: " + folderBrowser.SelectedPath);
-                currentPath = folderBrowser.SelectedPath;
-                SavePathToFile();
-                GetSubDirectories();
-                return folderBrowser.SelectedPath;
+                if (Directory.Exists(folderBrowser.SelectedPath))
+                {
+                    //Console.WriteLine("Set Path to: " + folderBrowser.SelectedPath);
+                    currentPath = folderBrowser.SelectedPath;
+                    SavePathToFile();
+                    GetSubDirectories();
+                    return folderBrowser.SelectedPath;
+                }
+                else
+                {
+                    //Console.WriteLine("Choosen Directory doesnt exist");
+                    return string.Empty;
+                }
             }
-            else
-            {
-                //Console.WriteLine("Choosen Directory doesnt exist");
-                return string.Empty;
-            }
+            return currentPath;
         }
 
         public string PathFromFile()
         {
-            if(File.ReadAllText(SavedPathFile) != string.Empty)
+            if(File.ReadAllLines(SavedPathFile).Length > 0)
             {
-                return File.ReadAllText(SavedPathFile);
+                return File.ReadAllLines(SavedPathFile)[0];
             }
             else
             {
@@ -85,7 +95,8 @@ namespace WDocAuto
         public void SavePathToFile()
         {
             //Console.WriteLine("Saving Path to Path File: " + SavedPathFile);
-            File.WriteAllText(SavedPathFile, currentPath);
+            string[] content = { currentPath, FolderInTitle.ToString(), TitleSize.ToString(), IncludeFDate.ToString() };
+            File.WriteAllLines(SavedPathFile, content);
         }
 
         public void GetSubDirectories()
@@ -108,12 +119,24 @@ namespace WDocAuto
             }
         }
 
+        public void UpdateOptions()
+        {
+            string[] options = File.ReadAllLines(SavedPathFile);
+            FolderInTitle = Convert.ToBoolean(options[1]);
+            TitleSize = Convert.ToInt32(options[2]);
+            IncludeFDate = Convert.ToBoolean(options[3]);
+            //Console.WriteLine("Path = " + currentPath);
+            //Console.WriteLine("FolderInTitle = " + FolderInTitle);
+            //Console.WriteLine("Title Size = " + TitleSize);
+            //Console.WriteLine("Include Folder Name and Date = " + IncludeFDate);
+        }
+
         public void SaveFileToCurrent(Word._Document FileToSave)
         {
            
         }
 
-        public void OptionsButtonClick(object sender, EventArgs e)
+        public void ChangeButtonClick(object sender, EventArgs e)
         {
             currentPath = ChangePathDialog();
         }
@@ -125,7 +148,8 @@ namespace WDocAuto
 
         public void CreateSaveDocument()
         {
-            string DatePath = PathNameComboBox.SelectedValue + " " + DateDay + "." + DateMonth + ".";
+            string DatePath = PathNameComboBox.SelectedValue + " " + DateDay + "." + DateMonth + "." + DateYear;
+            string DateOnly = DateDay + "." + DateMonth + "." + DateYear;
 
             object oMissing = System.Reflection.Missing.Value;
             object oEndOfDoc = "\\endofdoc";
@@ -133,21 +157,25 @@ namespace WDocAuto
             string TitleText = TitleTextBox.Text;
 
             Word._Application oWord;
-            Word._Document oDoc;
+            Word.Document oDoc;
             oWord = new Word.Application();
             oWord.Visible = true;
             oDoc = oWord.Documents.Add(ref oMissing, ref oMissing,
-            ref oMissing, ref oMissing);
+            0, ref oMissing);
 
-            Word.Paragraph Fach;
-            Fach = oDoc.Content.Paragraphs.Add(ref oMissing);
-            Fach.Range.Text = DatePath + DateTime.Now.Year.ToString().Substring(2);
-            Fach.Format.Alignment = Word.WdParagraphAlignment.wdAlignParagraphRight;
-            Fach.Range.InsertParagraphAfter();
+            if (IncludeFDate)
+            {
+                Word.Paragraph Fach;
+                Fach = oDoc.Content.Paragraphs.Add(ref oMissing);
+                Fach.Range.Text = DatePath;
+                Fach.Format.Alignment = Word.WdParagraphAlignment.wdAlignParagraphRight;
+                Fach.Range.InsertParagraphAfter();
+            }
 
             Word.Paragraph US;
             US = oDoc.Content.Paragraphs.Add(ref oMissing);
             US.Range.Text = TitleText;
+            US.Range.Font.Size = TitleSize;
             US.Format.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
             US.Range.Font.Bold = 1;
             US.Range.InsertParagraphAfter();
@@ -163,12 +191,19 @@ namespace WDocAuto
             {
                 currentPath = ChangePathDialog();
             }
-            oDoc.SaveAs(currentPath + "\\" + SelectedPath + "\\" + DatePath + DateTime.Now.Year.ToString().Substring(2) + " " + TitleText);
+            //Console.WriteLine(oDoc.SaveFormat);
+            oDoc.SaveAs(currentPath + "\\" + SelectedPath + "\\" + (FolderInTitle? DatePath : DateOnly) + " " + TitleText + ".docx", Word.WdSaveFormat.wdFormatDocumentDefault);
         }
 
         public void PathNameSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             SelectedPath = (string)PathNameComboBox.SelectedValue;
+        }
+
+        private void OptionsButtonClick(object sender, RoutedEventArgs e)
+        {
+            Window optionsWindow = new OptionsWindow(this);
+            optionsWindow.Show();
         }
     }
 }
---
+++ WDocAuto/OptionsWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Shapes;
+
+namespace WDocAuto
+{
+    /// <summary>
+    /// Interaktionslogik für OptionsWindow.xaml
+    /// </summary>
+    public partial class OptionsWindow : Window
+    {
+        public MainWindow mainWindow;
+        bool FolderInTitle = false;
+        bool IncludeDateTop = true;
+        int TitleSize = 11;
+        public OptionsWindow(MainWindow mainWindow)
+        {
+            //Lines
+            //1 = Path (string)
+            //2 = Folder Name in File Name (bool)
+            //3 = Size of Title Text (int)
+            //4 = Include Date on Top (bool)
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+            if(File.ReadAllLines(mainWindow.SavedPathFile).Length != 4 )
+            {
+                string[] write = { File.ReadAllLines(mainWindow.SavedPathFile)[0], FolderInTitle.ToString(), TitleSize.ToString(), IncludeDateTop.ToString()};
+                File.WriteAllLines(mainWindow.SavedPathFile, write);
+            }
+            GetOptions();
+        }
+
+        public void GetOptions()
+        {
+            string[] options = File.ReadAllLines(mainWindow.SavedPathFile);
+            FolderInTitle = Convert.ToBoolean(options[1]);
+            TitleSize = Convert.ToInt32(options[2]);
+            IncludeDateTop = Convert.ToBoolean(options[3]);
+            this.IncludeInNameBox.IsChecked = FolderInTitle;
+            this.TitleSizeBox.Text = TitleSize.ToString();
+            this.IncludeFDateBox.IsChecked = IncludeDateTop;
+        }
+
+        private void ApplyButtonClick(object sender, RoutedEventArgs e)
+        {
+            string[] content = File.ReadAllLines(mainWindow.SavedPathFile);
+            string[] toWrite = { content[0], FolderInTitle.ToString(), TitleSize.ToString(), IncludeDateTop.ToString() };
+            File.WriteAllLines(mainWindow.SavedPathFile, toWrite);
+            mainWindow.UpdateOptions();
+            this.Close();
+        }
+
+        private void InNameClick(object sender, RoutedEventArgs e)
+        {
+            FolderInTitle = Convert.ToBoolean(IncludeInNameBox.IsChecked.ToString());
+        }
+
+        private void TitleSizeBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TitleSizeTextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                TitleSize = Convert.ToInt32(TitleSizeBox.Text);
+            }
+            catch
+            {
+                if(TitleSizeBox.Text != string.Empty)
+                {
+                    TitleSizeBox.Clear();
+                    MessageBox.Show("Please enter a number");
+                }
+            }
+        }
+
+        private void IncludeFDateClick(object sender, RoutedEventArgs e)
+        {
+            IncludeDateTop = Convert.ToBoolean(IncludeFDateBox.IsChecked.ToString());
+        }
+    }
+}
